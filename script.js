c5bc8c5ba07e85b8e47b99b836fa7f75de60de55
dhbotond <dhbotond2005@gmail.com>
2024-09-29 17:22:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("contactButton").addEventListener("click", function() {
+    document.getElementById("contactMessage").innerText = "Kérlek, vedd fel velem a kapcsolatot az alábbi e-mail címen: dhbotond2005@gmail.com";
+});
